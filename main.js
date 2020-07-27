406d82ae8f3a50a85927f83a975884792aae3486
deybyr647 <deybyr647@gmail.com>
2020-07-26 22:23:32 -0400

    Lots of changes and additions. See comments under this commit

--- main.js
+++ main.js
@@ -1,44 +1,69 @@
 //Blinking Text
-let i = 0;
-
+let t = 0;
 let adjectiveText = () => {
     
     let adjectives = ['Web Developer', 'Insomniac', 'Musicophile', 'Stubborn', 'Introverted']
 
-    document.getElementById('blinktext').innerHTML = adjectives[i];
+    document.getElementById('blinktext').innerHTML = adjectives[t];
 
-    i++;
+    t++;
 
-    i >= adjectives.length - 1 ? i = 0 : i = i;
+    t >= adjectives.length - 1 ? t = 0 : t = t;
 }
 
 window.onload = () => {
     adjectiveText();
+
+    setInterval(() =>{
+        adjectiveText();
+    }, 2500)
 }
 
-setInterval(() =>{
-    adjectiveText();
-}, 2000)
-
 //Resume Modal
-
-let resume_modal = document.getElementById('resumeModal');
-
-let resume_icon = document.getElementById('resumeIcon');
-
-let modal_image = document.getElementById('resume');
-
+let resumeModal = document.getElementById('resume-modal');
+let resumeIcon = document.getElementById('resume-icon');
+let modalContent = document.getElementById('resume');
 let captionText = document.getElementById('resumeCaption');
 
-resume_icon.onclick = () => {
-    resume_modal.style.display = "block";
-    modal_image.src = 'assets/Resume.pdf';
+resumeIcon.onclick = () => {
+    resumeModal.style.display = "block";
+    modalContent.src = 'assets/Resume.pdf';
     captionText.innerHTML = 'My Resume';
-
 }
 
 let close = document.getElementById('close');
 
 close.onclick = () => {
-    resume_modal.style.display = 'none';
+    resumeModal.style.display = 'none';
+}
+
+//Show portfolio
+let portfolioIcon = document.getElementById('portfolio-icon');
+let portfolioContainer = document.getElementById('portfolio-container');
+let isShowing = false;
+
+portfolioIcon.onclick = () => {
+    isShowing = !isShowing;
+    isShowing == true ? portfolioContainer.style.display = 'flex' : portfolioContainer.style.display = 'none';
+}
+
+//Back to top button
+let back2Top = document.getElementById('toTop');
+
+let scrollFunc = () => {
+    document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ? back2Top.style.display = "block" : back2Top.style.display = "none";
+}
+
+window.onscroll = () => {
+    scrollFunc();
+}
+
+back2Top.onclick = () => {
+    isShowing = !isShowing;
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+
+    setTimeout(() => {
+        isShowing == true ? portfolioContainer.style.display = 'flex' : portfolioContainer.style.display = 'none';
+    }, 500)
 }
