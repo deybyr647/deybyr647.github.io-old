18a97b28f92ba2b84da750b2888befa94dee09a1
deybyr647 <deybyr647@gmail.com>
2020-08-08 02:02:18 -0400

    Began implementing dark mode toggle

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let adjectiveText = () => {
 
     let adjectives = ['Web Developer', 'Introverted', 'Freelancer', 'Dedicated', 'Business Inquisitive', 'Assertive', 'Finance Enthusiast', 'Leading'];
 
-    document.getElementById('blinktext').innerHTML = adjectives[t];
+    document.querySelector('#blinktext').innerHTML = adjectives[t];
 
     t++;
 
@@ -20,10 +20,10 @@ window.onload = () => {
 }
 
 //Resume Modal
-let resumeModal = document.getElementById('resume-modal');
-let resumeIcon = document.getElementById('resume-icon');
-let modalContent = document.getElementById('resume');
-let captionText = document.getElementById('resumeCaption');
+let resumeModal = document.querySelector('#resume-modal');
+let resumeIcon = document.querySelector('#resume-icon');
+let modalContent = document.querySelector('#resume');
+let captionText = document.querySelector('#resumeCaption');
 
 resumeIcon.onclick = () => {
     resumeModal.style.display = "block";
@@ -31,15 +31,15 @@ resumeIcon.onclick = () => {
     captionText.innerHTML = 'My Resume';
 }
 
-let close = document.getElementById('close');
+let close = document.querySelector('#close');
 
 close.onclick = () => {
     resumeModal.style.display = 'none';
 }
 
 //Show portfolio
-let portfolioIcon = document.getElementById('portfolio-icon');
-let portfolioContainer = document.getElementById('portfolio-container');
+let portfolioIcon = document.querySelector('#portfolio-icon');
+let portfolioContainer = document.querySelector('#portfolio-container');
 let isShowing = false;
 
 portfolioIcon.onclick = () => {
@@ -48,10 +48,11 @@ portfolioIcon.onclick = () => {
 }
 
 //Back to top button
-let back2Top = document.getElementById('toTop');
+let floatingIcons = document.querySelector('.floatingIcons');
+let back2Top = document.querySelector('#toTop');
 
 let scrollFunc = () => {
-    document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ? back2Top.style.display = "block" : back2Top.style.display = "none";
+    document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ? floatingIcons.style.display = "block" : floatingIcons.style.display = "none";
 }
 
 window.onscroll = () => scrollFunc();
@@ -64,4 +65,45 @@ back2Top.onclick = () => {
     setTimeout(() => {
         isShowing == true ? portfolioContainer.style.display = 'flex' : portfolioContainer.style.display = 'none';
     }, 500)
+}
+
+
+//Dark Mode Toggle
+let darkModeToggle = document.querySelector('#darkModeToggle');
+let modeIcon = document.querySelector('#modeIcon');
+let isEnabled = false;
+
+let bodyColor = document.querySelector('body').style.backgroundColor;
+let aboutSkillsContainerColor = document.querySelector('.about-skills-container').style.backgroundColor;
+let portfolioContainerColor = document.querySelector('#portfolio-container').style.backgroundColor;
+let nameColor = document.querySelector('.name').style.backgroundColor;
+
+bodyColor = '#E8D7F1';
+aboutSkillsContainerColor = 'thistle';
+portfolioContainerColor = 'thistle';
+nameColor = '#4F359B';
+
+let toggleMode = () => {
+
+    if(isEnabled == true){
+        modeIcon.className = 'fas fa-moon'
+        bodyColor = '#7F5A83';
+        aboutSkillsContainerColor = '#2C0735';
+        portfolioContainerColor = '#2C0735';
+        nameColor = '#000000';
+    } else{
+        modeIcon.className = 'fas fa-sun';
+        bodyColor = '#E8D7F1';
+        aboutSkillsContainerColor = 'thistle';
+        portfolioContainerColor = 'thistle';
+        nameColor = '#4F359B';
+    }
+}
+
+darkModeToggle.onclick = () => {
+    isEnabled = !isEnabled;
+
+    document.querySelector('body').style.backgroundColor = 'black';
+
+    console.log(isEnabled);
 }
